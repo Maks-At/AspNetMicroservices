071e0c380601c09a5bb41e32362af35d6d6e7084
Maks_At <maks.atagoziyev@gmail.com>
2022-12-06 13:20:40 +0300

    Resolve dependencies

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,5 @@
+using Discount.API.Repositories;
+
 namespace Discount.API
 {
     public class Program
@@ -7,7 +9,7 @@ namespace Discount.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
